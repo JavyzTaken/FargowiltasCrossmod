aa4cddaca73d94354603dc01d8693b9e53c82946
Galster-dev <46283010+Galster-dev@users.noreply.github.com>
2023-08-27 15:39:28 +0300

    Organize weak references

--- FargowiltasCrossmod.cs
+++ FargowiltasCrossmod.cs
@@ -1,8 +1,23 @@
+using System.Linq;
+using System.Reflection;
 using Terraria.ModLoader;
 
-namespace FargowiltasCrossmod
+namespace FargowiltasCrossmod;
+
+public class FargowiltasCrossmod : Mod
 {
-	public class FargowiltasCrossmod : Mod
-	{
-	}
+    private class ClassPreJitFilter : PreJITFilter
+    {
+        public override bool ShouldJIT(MemberInfo member)
+        {
+            return base.ShouldJIT(member) &&
+                   // also check attributes on declaring class
+                   member.DeclaringType?.GetCustomAttributes<MemberJitAttribute>().All(a => a.ShouldJIT(member)) != false;
+        }
+    }
+
+    public FargowiltasCrossmod()
+    {
+        PreJITFilter = new ClassPreJitFilter();
+    }
 }
