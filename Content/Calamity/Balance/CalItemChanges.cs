cbbf6d6f95b91905ee68f62fb4037cef90a7b3f3
JavyzTaken <jacklukas@outlook.com>
2023-08-28 02:35:46 +0200

    Made Celestial Onion an upgrade to Mutant's Pact instead

--- Content/Calamity/Balance/CalItemChanges.cs
+++ Content/Calamity/Balance/CalItemChanges.cs
@@ -1,7 +1,9 @@
-﻿using CalamityMod.Items.PermanentBoosters;
+﻿using CalamityMod.CalPlayer;
+using CalamityMod.Items.PermanentBoosters;
 using FargowiltasCrossmod.Core;
 using FargowiltasCrossmod.Core.Calamity;
 using FargowiltasSouls.Content.Items.Consumables;
+using FargowiltasSouls.Core.ModPlayers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Terraria;
 using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
 
 namespace FargowiltasCrossmod.Content.Calamity.Balance
 {
@@ -19,7 +22,7 @@ namespace FargowiltasCrossmod.Content.Calamity.Balance
         {
             if (item.type == ModContent.ItemType<CelestialOnion>() && CalamityConfig.Instance.BalanceChanges)
             {
-                return false;
+                return player.GetModPlayer<FargoSoulsPlayer>().MutantsPactSlot;
             }
             return true;
         }
@@ -27,8 +30,37 @@ namespace FargowiltasCrossmod.Content.Calamity.Balance
         {
             if (item.type == ModContent.ItemType<CelestialOnion>() && CalamityConfig.Instance.BalanceChanges)
             {
-                tooltips.Add(new TooltipLine(Mod, "OnionDisabled", $"[c/FF0000:Disabled by the \"Calamity/Souls Balance changes\" config. \nUse the ][i:{ModContent.ItemType<MutantsPact>()}][c/FF0000:Mutant's Pact for an additional accessory slot.]"));
+                tooltips.Add(new TooltipLine(Mod, "OnionDisabled", $"[c/FF0000:\"Calamity/Souls Balance changes\":] Is now an upgrade to [i:{ModContent.ItemType<MutantsPact>()}]Mutant's Pact, that allows any accessory in the extra slot."));
             }
+        }
+    }
+    [JITWhenModsEnabled(ModCompatibility.Calamity.Name)]
+    public class CalExtraSlotPlayer : ModPlayer
+    {
+        public bool MutantPactShouldBeEnabled = false;
+        public override void PostUpdate()
+        {
+            ref bool MutantsPactSlot = ref Player.GetModPlayer<FargoSoulsPlayer>().MutantsPactSlot;
+            if (Player.GetModPlayer<CalamityPlayer>().extraAccessoryML && CalamityConfig.Instance.BalanceChanges) 
+            {
+                if (MutantsPactSlot)
+                {
+                    MutantPactShouldBeEnabled = true; //store if the slot is enabled
+                    MutantsPactSlot = false; //turn it off since celestial onion slot is replacing it
+                }
+            }
+            else if (MutantPactShouldBeEnabled)
+            {
+                MutantsPactSlot = true;
+            }
+        }
+        public override void SaveData(TagCompound tag)
+        {
+            tag.Add($"{Mod.Name}.{Player.name}.MutantPactShouldBeEnabled", MutantPactShouldBeEnabled);
+        }
+        public override void LoadData(TagCompound tag)
+        {
+            MutantPactShouldBeEnabled = tag.GetBool($"{Mod.Name}.{Player.name}.MutantPactShouldBeEnabled");
         }
     }
 }
