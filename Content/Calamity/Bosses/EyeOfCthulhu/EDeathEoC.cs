cae6196f0a941a7dd7dcd95a71b4dd7d77996b93
JavyzTaken <jacklukas@outlook.com>
2023-09-25 20:30:23 +0200

    Moved EoC folder to new Bosses folder

--- Content/Calamity/Bosses/EyeOfCthulhu/EDeathEoC.cs
+++ Content/Calamity/Bosses/EyeOfCthulhu/EDeathEoC.cs
@@ -23,7 +23,7 @@ using Terraria.ModLoader;
 using CalamityMod.Events;
 using FargowiltasSouls.Content.Projectiles.Masomode;
 
-namespace FargowiltasCrossmod.Content.Calamity.NPCS.Vanilla.EyeOfCthulhu
+namespace FargowiltasCrossmod.Content.Calamity.Bosses.EyeOfCthulhu
 {
     [ExtendsFromMod(ModCompatibility.Calamity.Name)]
     public class EDeathEoC : EternideathNPC
@@ -91,7 +91,7 @@ namespace FargowiltasCrossmod.Content.Calamity.NPCS.Vanilla.EyeOfCthulhu
         const int WindupTime = 100;
         private void HorizontalDash(NPC npc) //i stole calamity horizontal dash ai and adapted it a bit
         {
-            float lifeRatio = (float)npc.life / (float)npc.lifeMax;
+            float lifeRatio = npc.life / (float)npc.lifeMax;
             float br = BossRushEvent.BossRushActive ? 1f : 0f;
             if (npc.ai[1] == 15)
             {
@@ -106,7 +106,7 @@ namespace FargowiltasCrossmod.Content.Calamity.NPCS.Vanilla.EyeOfCthulhu
                 num40 = Side == -1 ? -500f : 500f;
                 num43 *= 1.5f;
                 num44 *= 1.5f;
-                destination = (Main.player[npc.target]).Center + Vector2.UnitX * num40;
+                destination = Main.player[npc.target].Center + Vector2.UnitX * num40;
                 Vector2 val13 = npc.SafeDirectionTo(destination) * num43;
                 npc.rotation = Rotate(npc, npc.rotation + MathHelper.PiOver2, Main.player[npc.target].Center, 2) - MathHelper.PiOver2;
                 npc.SimpleFlyMovement(val13, num44);
@@ -115,16 +115,16 @@ namespace FargowiltasCrossmod.Content.Calamity.NPCS.Vanilla.EyeOfCthulhu
                 int eyeDelay = FargowiltasSouls.Core.Systems.WorldSavingSystem.MasochistModeReal ? 2 : 45; //funny old gigavomit crossmod bug on maso
                 if (npc.ai[2] % eyeDelay == 0f)
                 {
-                    Vector2 val14 = Vector2.Normalize((Main.player[npc.target]).Center - (npc).Center) * 5f;
-                    Vector2 val15 = (npc).Center + val14 * 10f;
+                    Vector2 val14 = Vector2.Normalize(Main.player[npc.target].Center - npc.Center) * 5f;
+                    Vector2 val15 = npc.Center + val14 * 10f;
                     if (Main.netMode != NetmodeID.MultiplayerClient)
                     {
-                        int num45 = NPC.NewNPC((npc).GetSource_FromAI((string)null), (int)val15.X, (int)val15.Y, 5, 0, 0f, 0f, 0f, 0f, 255);
-                        (Main.npc[num45]).velocity.X = val14.X;
-                        (Main.npc[num45]).velocity.Y = val14.Y;
+                        int num45 = NPC.NewNPC(npc.GetSource_FromAI(null), (int)val15.X, (int)val15.Y, 5, 0, 0f, 0f, 0f, 0f, 255);
+                        Main.npc[num45].velocity.X = val14.X;
+                        Main.npc[num45].velocity.Y = val14.Y;
                         if (Main.netMode == NetmodeID.Server && num45 < Main.maxNPCs)
                         {
-                            NetMessage.SendData(MessageID.SyncNPC, -1, -1, (NetworkText)null, num45, 0f, 0f, 0f, 0, 0, 0);
+                            NetMessage.SendData(MessageID.SyncNPC, -1, -1, null, num45, 0f, 0f, 0f, 0, 0, 0);
                         }
                         if (CalamityWorld.LegendaryMode)
                         {
@@ -134,15 +134,15 @@ namespace FargowiltasCrossmod.Content.Calamity.NPCS.Vanilla.EyeOfCthulhu
                             float num48 = MathHelper.ToRadians(20f);
                             for (int n = 0; n < num47; n++)
                             {
-                                Vector2 val17 = Utils.RotatedBy(val16, (double)MathHelper.Lerp(0f - num48, num48, (float)n / (float)(num47 - 1)), default(Vector2));
-                                Projectile.NewProjectile((npc).GetSource_FromAI((string)null), (npc).Center + Vector2.Normalize(val17) * 10f, val17, num46, 15, 0f, Main.myPlayer, 0f, 0f, 0f);
+                                Vector2 val17 = val16.RotatedBy((double)MathHelper.Lerp(0f - num48, num48, n / (float)(num47 - 1)), default);
+                                Projectile.NewProjectile(npc.GetSource_FromAI(null), npc.Center + Vector2.Normalize(val17) * 10f, val17, num46, 15, 0f, Main.myPlayer, 0f, 0f, 0f);
                             }
                         }
                     }
-                    SoundEngine.PlaySound(SoundID.NPCDeath13, (Vector2?)(npc).position, (SoundUpdateCallback)null);
+                    SoundEngine.PlaySound(SoundID.NPCDeath13, (Vector2?)npc.position, null);
                     for (int num49 = 0; num49 < 10; num49++)
                     {
-                        Dust.NewDust(val15, 20, 20, 5, val14.X * 0.4f, val14.Y * 0.4f, 0, default(Color), 1f);
+                        Dust.NewDust(val15, 20, 20, 5, val14.X * 0.4f, val14.Y * 0.4f, 0, default, 1f);
                     }
                 }
                 float num50 = WindupTime;
@@ -166,10 +166,10 @@ namespace FargowiltasCrossmod.Content.Calamity.NPCS.Vanilla.EyeOfCthulhu
                     float num51 = 6f * (0.4f - lifeRatio);
                     float num52 = 18f + num51;
                     num52 += 10f * br;
-                    (npc).velocity = npc.SafeDirectionTo((Main.player[npc.target]).Center) * num52;
+                    npc.velocity = npc.SafeDirectionTo(Main.player[npc.target].Center) * num52;
                     npc.ai[1] = 17;
                     npc.netUpdate = true;
-                    
+
                     if (npc.netSpam > 10)
                     {
                         npc.netSpam = 10;
@@ -180,7 +180,7 @@ namespace FargowiltasCrossmod.Content.Calamity.NPCS.Vanilla.EyeOfCthulhu
             {
                 if (npc.ai[2] == 0)
                 {
-                    SoundEngine.PlaySound(SoundID.Roar,npc.position, null);
+                    SoundEngine.PlaySound(SoundID.Roar, npc.position, null);
                 }
 
                 float dashTime = 40;
@@ -190,25 +190,25 @@ namespace FargowiltasCrossmod.Content.Calamity.NPCS.Vanilla.EyeOfCthulhu
                         FargoSoulsUtil.XWay(8, npc.GetSource_FromThis(), npc.Center, ModContent.ProjectileType<BloodScythe>(), 1.5f, FargoSoulsUtil.ScaledProjectileDamage(npc.damage), 0);
                 }
                 npc.ai[2] += 1;
-                if (npc.ai[2] == dashTime && Vector2.Distance((npc).position, (Main.player[npc.target]).position) < 200f)
+                if (npc.ai[2] == dashTime && Vector2.Distance(npc.position, Main.player[npc.target].position) < 200f)
                 {
                     npc.ai[2] -= 1;
                 }
                 if (npc.ai[2] >= dashTime)
                 {
-                    (npc).velocity = (npc).velocity * 0.95f;
-                    if ((npc).velocity.X > -0.1 && (double)(npc).velocity.X < 0.1)
+                    npc.velocity = npc.velocity * 0.95f;
+                    if (npc.velocity.X > -0.1 && npc.velocity.X < 0.1)
                     {
-                        (npc).velocity.X = 0f;
+                        npc.velocity.X = 0f;
                     }
-                    if ((npc).velocity.Y > -0.1 && (double)(npc).velocity.Y < 0.1)
+                    if (npc.velocity.Y > -0.1 && npc.velocity.Y < 0.1)
                     {
-                        (npc).velocity.Y = 0f;
+                        npc.velocity.Y = 0f;
                     }
                 }
                 else
                 {
-                    npc.rotation = Utils.ToRotation((npc).velocity) - (float)Math.PI / 2f;
+                    npc.rotation = npc.velocity.ToRotation() - (float)Math.PI / 2f;
                 }
                 float endTime = dashTime + 13f;
                 if (npc.ai[2] >= endTime)
@@ -228,7 +228,7 @@ namespace FargowiltasCrossmod.Content.Calamity.NPCS.Vanilla.EyeOfCthulhu
         {
             Vector2 PV = npc.DirectionTo(target);
             Vector2 LV = rotation.ToRotationVector2();
-            float anglediff = (float)(Math.Atan2(PV.Y * LV.X - PV.X * LV.Y, LV.X * PV.X + LV.Y * PV.Y)); //real
+            float anglediff = (float)Math.Atan2(PV.Y * LV.X - PV.X * LV.Y, LV.X * PV.X + LV.Y * PV.Y); //real
             //change rotation towards target
             return rotation.ToRotationVector2().RotatedBy(Math.Sign(anglediff) * Math.Min(Math.Abs(anglediff), speed * MathHelper.Pi / 180)).ToRotation();
         }
