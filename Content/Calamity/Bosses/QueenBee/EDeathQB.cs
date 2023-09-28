075ea056f7e4347e562ddd9b74ef993dd7c6aa4b
Shipmans_AGLA <boyblob@gmail.com>
2023-09-27 21:42:13 -0700

    EDeath boc, eow, destroyer, qb, wof

---
+++ Content/Calamity/Bosses/QueenBee/EDeathQB.cs
@@ -0,0 +1,30 @@
+﻿using FargowiltasSouls.Content.Bosses.VanillaEternity;
+using FargowiltasSouls.Core.Globals;
+using FargowiltasSouls.Core.NPCMatching;
+using System.IO;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader.IO;
+using Microsoft.Xna.Framework;
+
+namespace FargowiltasCrossmod.Content.Calamity.Bosses.EaterofWorlds
+{
+    public class EDeathQB : EternideathNPC
+    {
+        public override NPCMatcher CreateMatcher() => new NPCMatcher().MatchType(NPCID.QueenBee);
+        public override void SendExtraAI(NPC npc, BitWriter bitWriter, BinaryWriter binaryWriter)
+        {
+            base.SendExtraAI(npc, bitWriter, binaryWriter);
+        }
+        public override void ReceiveExtraAI(NPC npc, BitReader bitReader, BinaryReader binaryReader)
+        {
+            base.ReceiveExtraAI(npc, bitReader, binaryReader);
+        }
+        public override bool SafePreAI(NPC npc)
+        {
+            if (!npc.HasValidTarget) return true;
+            npc.position += npc.velocity * 0.75f;
+            return base.SafePreAI(npc);
+        }
+    }
+}
---
+++ Content/Calamity/Bosses/WallofFlesh/HomingSickle.cs
@@ -0,0 +1,52 @@
+﻿using CalamityMod.NPCs.TownNPCs;
+using FargowiltasSouls;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FargowiltasCrossmod.Content.Calamity.Bosses.WallofFlesh
+{
+    public class HomingSickle : ModProjectile
+    {
+        public override string Texture => "Terraria/Images/Projectile_" + ProjectileID.DemonScythe;
+        public override void SetDefaults()
+        {
+            Projectile.width = 40;
+            Projectile.height = 40;
+            Projectile.hostile = true;
+            Projectile.timeLeft = 1000;
+        }
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Asset<Texture2D> t = TextureAssets.Projectile[Type];
+            Main.EntitySpriteDraw(t.Value, Projectile.Center - Main.screenPosition, null, lightColor, Projectile.rotation, t.Size() / 2, Projectile.scale, SpriteEffects.None);
+            return false;
+        }
+        public override void Kill(int timeLeft)
+        {
+            SoundEngine.PlaySound(SoundID.NPCHit3, Projectile.Center);
+        }
+        public override void AI()
+        {
+            Projectile.ai[0]++;
+            Projectile.rotation += MathHelper.ToRadians(30);
+            Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.DemonTorch).noGravity = true;
+            if (Projectile.velocity.Length() < 15) Projectile.velocity *= 1.05f;
+            int p = Player.FindClosest(Projectile.Center, 1, 1);
+            if (Projectile.ai[0] > 30 && Projectile.ai[0] < 130 && p >= 0)
+            {
+                Projectile.velocity = new Vector2(Projectile.velocity.Length(), 0).RotatedBy(Utils.AngleTowards(Projectile.velocity.ToRotation(), Projectile.AngleTo(Main.player[p].Center), 0.02f));
+            }
+        }
+    }
+}
