1a2dd684df34709c12f2d3b794b37b5ac74af125
JavyzTaken <jacklukas@outlook.com>
2023-10-03 17:01:01 +0200

    Fixed missed ExtendsFromMod(Calamity)

--- Content/Calamity/Bosses/Perforators/PerfProjectilesEternity.cs
+++ Content/Calamity/Bosses/Perforators/PerfProjectilesEternity.cs
@@ -1,4 +1,5 @@
 ﻿using CalamityMod.Projectiles.Boss;
+using FargowiltasCrossmod.Core;
 using FargowiltasCrossmod.Core.Systems;
 using FargowiltasSouls.Core.Systems;
 using Microsoft.Xna.Framework;
@@ -12,6 +13,8 @@ using Terraria.ModLoader;
 
 namespace FargowiltasCrossmod.Content.Calamity.Bosses.Perforators
 {
+    [JITWhenModsEnabled(ModCompatibility.Calamity.Name)]
+    [ExtendsFromMod(ModCompatibility.Calamity.Name)]
     public class PerfProjectilesEternity : GlobalProjectile
     {
         public override bool TileCollideStyle(Projectile projectile, ref int width, ref int height, ref bool fallThrough, ref Vector2 hitboxCenterFrac)
