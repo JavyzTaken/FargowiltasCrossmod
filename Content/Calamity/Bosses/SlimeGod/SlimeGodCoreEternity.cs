d67ab232f6340bb711ef6d590fc5cb09bca41e78
JavyzTaken <jacklukas@outlook.com>
2023-10-03 23:54:33 +0200

    Started Slime God rework Supersonic and Flight Mastery Souls now require Auric Bars instead of Abominable Energy

---
+++ Content/Calamity/Bosses/SlimeGod/SlimeGodCoreEternity.cs
@@ -0,0 +1,253 @@
+﻿
+using CalamityMod.Events;
+using CalamityMod.NPCs;
+using CalamityMod.NPCs.DesertScourge;
+using CalamityMod.NPCs.SlimeGod;
+using CalamityMod.Particles;
+using CalamityMod.Projectiles.Boss;
+using FargowiltasCrossmod.Core;
+using FargowiltasCrossmod.Core.Utils;
+using FargowiltasSouls;
+using FargowiltasSouls.Core.Globals;
+using FargowiltasSouls.Core.NPCMatching;
+using FargowiltasSouls.Core.Systems;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using System;
+using System.IO;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace FargowiltasCrossmod.Content.Calamity.Bosses.SlimeGod
+{
+    [JITWhenModsEnabled(ModCompatibility.Calamity.Name)]
+    [ExtendsFromMod(ModCompatibility.Calamity.Name)]
+    public class SlimeGodCoreEternity : EModeCalBehaviour
+    {
+        public override NPCMatcher CreateMatcher() => new NPCMatcher().MatchType(ModContent.NPCType<SlimeGodCore>());
+
+
+        #region Variables
+        public static int CrimsonGodType => ModContent.NPCType<CrimulanPaladin>();
+        public static int CorruptionGodType => ModContent.NPCType<EbonianPaladin>();
+        public bool AttachToCrimson = WorldGen.crimson; //start off with the current world evil
+        public int AttachedSlime;
+        public bool ContactDamage = false;
+        public enum Phases
+        {
+            ShouldAttach,
+            TryAttach,
+            Attached,
+            Attacking
+        }
+        #endregion
+        public override void SendExtraAI(NPC npc, BitWriter bitWriter, BinaryWriter binaryWriter)
+        {
+            binaryWriter.Write7BitEncodedInt(AttachedSlime);
+            binaryWriter.Write(AttachToCrimson);
+            binaryWriter.Write(ContactDamage);
+            
+        }
+        public override void ReceiveExtraAI(NPC npc, BitReader bitReader, BinaryReader binaryReader)
+        {
+            AttachToCrimson = binaryReader.ReadBoolean();
+            AttachedSlime = binaryReader.Read7BitEncodedInt();
+            ContactDamage = binaryReader.ReadBoolean();
+        }
+
+        public override bool CanHitPlayer(NPC npc, Player target, ref int cooldownSlot) => ContactDamage;
+
+        public override void SetDefaults(NPC entity)
+        {
+            base.SetDefaults(entity);
+        }
+        public override bool SafePreAI(NPC npc)
+        {
+            if (!WorldSavingSystem.EternityMode) return true;
+            #region Passive
+            //FargoSoulsUtil.PrintAI(npc);
+            CalamityGlobalNPC.slimeGod = npc.whoAmI;
+            ref float timer = ref npc.ai[0];
+            ref float phase = ref npc.ai[3];
+
+            npc.dontTakeDamage = !(phase == (int)Phases.Attacking);
+
+            if (phase != (int)Phases.Attached)
+            {
+                npc.Opacity = 1f;
+                npc.rotation += npc.velocity.Length() * (MathHelper.Pi / 300f);
+            }
+            else
+            {
+                npc.Opacity = 0.3f;
+            }
+
+            if (!Targeting())
+                return false;
+
+            switch (phase)
+            {
+                case (int)Phases.ShouldAttach:
+                    {
+                        SummonSlimes();
+                    }
+                    break;
+                case (int)Phases.TryAttach:
+                    {
+                        TryAttaching();
+                    }
+                    break;
+                case (int)Phases.Attached:
+                    {
+                        Attached();
+                    }
+                    break;
+                case (int)Phases.Attacking:
+                    {
+
+                    }
+                    break;
+            }
+            timer++;
+            return false;
+            #endregion
+            #region Common Methods
+            bool Targeting()
+            {
+                const float despawnRange = 5000f;
+                Player p = Main.player[npc.target];
+                if (!p.active || p.dead || Vector2.Distance(npc.Center, p.Center) > despawnRange)
+                {
+                    npc.TargetClosest();
+                    p = Main.player[npc.target];
+                    if (!p.active || p.dead || Vector2.Distance(npc.Center, p.Center) > despawnRange)
+                    {
+                        npc.noTileCollide = true;
+                        if (npc.timeLeft > 30)
+                            npc.timeLeft = 30;
+                        npc.velocity.Y -= 1f;
+                        if (npc.timeLeft == 1)
+                        {
+                            if (Main.netMode != NetmodeID.MultiplayerClient)
+                            {
+                                FargoSoulsUtil.ClearHostileProjectiles(2, npc.whoAmI);
+                            }
+                        }
+                        return false;
+                    }
+                }
+                return true;
+            }
+            #endregion
+            #region Special Actions
+            void SummonSlimes()
+            {
+                ref float timer = ref npc.ai[0];
+                ref float phase = ref npc.ai[3];
+                if (timer < 120)
+                {
+                    Vector2 desiredPos = Main.player[npc.target].Center - Vector2.UnitY * 300;
+                    npc.velocity = (desiredPos - npc.Center) * 0.05f;
+                }
+                if (timer >= 120)
+                {
+                    if (DLCUtils.HostCheck)
+                    {
+                        const int spawnTossSpeed = 30;
+                        if (!NPC.AnyNPCs(CrimsonGodType))
+                        {
+                            int crim = NPC.NewNPC(npc.GetSource_FromAI(), (int)npc.Center.X, (int)npc.Center.Y, CrimsonGodType, Target: npc.target);
+                            if (crim != Main.maxNPCs)
+                            {
+                                Main.npc[crim].velocity = Vector2.UnitX * -spawnTossSpeed;
+                                if (AttachToCrimson)
+                                {
+                                    AttachedSlime = crim;
+                                    phase = (int)Phases.TryAttach;
+                                    AttachToCrimson = !AttachToCrimson;
+                                    Reset();
+                                    NetSync(npc);
+                                    npc.netUpdate = true;
+                                }
+                            }
+                        }
+                        if (!NPC.AnyNPCs(CorruptionGodType))
+                        {
+                            int corr = NPC.NewNPC(npc.GetSource_FromAI(), (int)npc.Center.X, (int)npc.Center.Y, CorruptionGodType, Target: npc.target);
+                            if (corr != Main.maxNPCs)
+                            {
+                                Main.npc[corr].velocity = Vector2.UnitX * spawnTossSpeed;
+                                if (!AttachToCrimson)
+                                {
+                                    AttachedSlime = corr;
+                                    phase = (int)Phases.TryAttach;
+                                    AttachToCrimson = !AttachToCrimson;
+                                    Reset();
+
+                                    NetSync(npc);
+                                    npc.netUpdate = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            void TryAttaching()
+            {
+                ref float timer = ref npc.ai[0];
+                ref float phase = ref npc.ai[3];
+                NPC slime = Main.npc[AttachedSlime];
+                ContactDamage = false;
+                if (slime != null && slime.active)
+                {
+                    float modifier = timer / 60f;
+                    npc.velocity = npc.DirectionTo(slime.Center) * 20f * modifier;
+                    if (npc.Distance(slime.Center) <= npc.velocity.Length() * 2f)
+                    {
+                        phase = (int)Phases.Attached;
+                        Reset();
+                    }
+                }
+                else
+                {
+                    Reset();
+                }
+            }
+            void Attached()
+            {
+                ref float timer = ref npc.ai[0];
+                ref float phase = ref npc.ai[3];
+                NPC slime = Main.npc[AttachedSlime];
+                ContactDamage = false;
+                if (slime != null && slime.active)
+                {
+                    npc.velocity = (slime.Center - npc.Center) + slime.velocity;
+                }
+                else
+                {
+
+                }
+            }
+            void SlimeBreakout()
+            {
+                ref float timer = ref npc.ai[0];
+                ref float phase = ref npc.ai[3];
+            }
+            void Reset()
+            {
+                npc.ai[0] = 0;
+                npc.ai[1] = 0;
+                npc.ai[2] = 0;
+            }
+            #endregion
+            #region Attacks
+            #endregion
+        }
+    }
+}
---
+++ Content/Calamity/Bosses/SlimeGod/SlimeGodsEternity.cs
@@ -0,0 +1,129 @@
+﻿
+using CalamityMod.Events;
+using CalamityMod.NPCs;
+using CalamityMod.NPCs.DesertScourge;
+using CalamityMod.NPCs.Perforator;
+using CalamityMod.NPCs.SlimeGod;
+using CalamityMod.Particles;
+using CalamityMod.Projectiles.Boss;
+using FargowiltasCrossmod.Core;
+using FargowiltasCrossmod.Core.Utils;
+using FargowiltasSouls;
+using FargowiltasSouls.Core.Globals;
+using FargowiltasSouls.Core.NPCMatching;
+using FargowiltasSouls.Core.Systems;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using System;
+using System.IO;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using Terraria.WorldBuilding;
+
+namespace FargowiltasCrossmod.Content.Calamity.Bosses.SlimeGod
+{
+    [JITWhenModsEnabled(ModCompatibility.Calamity.Name)]
+    [ExtendsFromMod(ModCompatibility.Calamity.Name)]
+    public class SlimeGodsEternity : EModeCalBehaviour
+    {
+        public override NPCMatcher CreateMatcher() => new NPCMatcher().MatchTypeRange(
+            ModContent.NPCType<EbonianPaladin>(),
+            ModContent.NPCType<CrimulanPaladin>()
+        );
+        //Slimes have much less health because of the phase and respawn mechanic
+        public override void SetDefaults(NPC npc)
+        {
+            base.SetDefaults(npc);
+            npc.lifeMax /= 4;
+        }
+        //Slime the Core is attached to takes 50% damage
+        public override void ModifyIncomingHit(NPC npc, ref NPC.HitModifiers modifiers)
+        {
+            NPC core = Main.npc[CalamityGlobalNPC.slimeGod];
+            if (core != null && core.active && core.type == ModContent.NPCType<SlimeGodCore>())
+            {
+                if (core.TryGetGlobalNPC(out SlimeGodCoreEternity emodeCore) && emodeCore.AttachedSlime == npc.whoAmI)
+                {
+                    modifiers.FinalDamage /= 2;
+                }
+            }
+        }
+        //Slime the Core is attached to draws a glow aura
+        public override bool PreDraw(NPC npc, SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+        {
+            NPC core = Main.npc[CalamityGlobalNPC.slimeGod];
+            if (core != null && core.active && core.type == ModContent.NPCType<SlimeGodCore>())
+            {
+                if (core.TryGetGlobalNPC(out SlimeGodCoreEternity emodeCore) && emodeCore.AttachedSlime == npc.whoAmI)
+                {
+                    //draw glow aura
+                    for (int j = 0; j < 12; j++)
+                    {
+                        Texture2D texture = TextureAssets.Npc[npc.type].Value;
+
+                        SpriteEffects effects = 0;
+                        if (npc.spriteDirection == 1)
+                        {
+                            effects = (SpriteEffects)1;
+                        }
+
+                        Vector2 afterimageOffset = (MathHelper.TwoPi * j / 12f).ToRotationVector2() * 6f;
+                        Color color = npc.type == ModContent.NPCType<EbonianPaladin>() ? Color.Purple : Color.Red;
+                        Color glowColor = color with { A = 0 } * 0.7f;
+
+                        Vector2 wackySlimeOffsetY = -Vector2.UnitY * npc.height * 0.125f;
+                        Vector2 offset = afterimageOffset + wackySlimeOffsetY - screenPos + new Vector2(0f, npc.gfxOffY);
+
+                        Main.EntitySpriteDraw(texture, npc.Center + offset, npc.frame, glowColor, npc.rotation, Utils.Size(npc.frame) / 2f, npc.scale, effects);
+                    }
+                }
+            }
+            return base.PreDraw(npc, spriteBatch, screenPos, drawColor);
+        }
+    }
+    public class EbonianPaladinEternity : EModeCalBehaviour
+    {
+        public override NPCMatcher CreateMatcher() => new NPCMatcher().MatchType(ModContent.NPCType<EbonianPaladin>());
+        public override void SetDefaults(NPC entity)
+        {
+            base.SetDefaults(entity);
+        }
+        public override bool SafePreAI(NPC npc)
+        {
+            return true;
+            if (!WorldSavingSystem.EternityMode) return true;
+            #region Passives and Variables
+            CalamityGlobalNPC.slimeGodPurple = npc.whoAmI;
+            NPC core = Main.npc[CalamityGlobalNPC.slimeGod];
+            #endregion
+            return false;
+        }
+
+    }
+    [JITWhenModsEnabled(ModCompatibility.Calamity.Name)]
+    [ExtendsFromMod(ModCompatibility.Calamity.Name)]
+    public class CrimuleanPaladinEternity : EModeCalBehaviour
+    {
+        public override NPCMatcher CreateMatcher() => new NPCMatcher().MatchType(ModContent.NPCType<CrimulanPaladin>());
+        public override void SetDefaults(NPC entity)
+        {
+            base.SetDefaults(entity);
+        }
+        public override bool SafePreAI(NPC npc)
+        {
+            return true;
+            if (!WorldSavingSystem.EternityMode) return true;
+            #region Passives and Variables
+            CalamityGlobalNPC.slimeGodRed = npc.whoAmI;
+            NPC core = Main.npc[CalamityGlobalNPC.slimeGod];
+            #endregion
+            return false;
+        }
+    }
+}
