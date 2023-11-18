9f411690e6e060801a49041e555e37271cc0ed8c
JavyzTaken <jacklukas@outlook.com>
2023-11-19 00:15:01 +0100

    """"resprited"""" Eye of Extinction Fixed Eye of Extinction boss spawning

--- Content/Calamity/Items/Summons/EyeofExtinction.cs
+++ Content/Calamity/Items/Summons/EyeofExtinction.cs
@@ -1,8 +1,19 @@
-﻿using CalamityMod.Items.Materials;
+﻿using CalamityMod;
+using CalamityMod.Items.Materials;
+using CalamityMod.Items.Potions.Alcohol;
+using CalamityMod.NPCs.DesertScourge;
 using CalamityMod.NPCs.SupremeCalamitas;
+using CalamityMod.Projectiles.Boss;
+using CalamityMod.Rarities;
+using CalamityMod.Tiles.Furniture.CraftingStations;
+using CalamityMod.World;
 using Fargowiltas.Items.Summons;
 using FargowiltasCrossmod.Core;
+using FargowiltasSouls;
+using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -17,6 +28,17 @@ namespace FargowiltasCrossmod.Content.Calamity.Items.Summons
         {
             Recipe.Create(Type).AddIngredient<AshesofCalamity>(10).AddIngredient<AuricBar>(4).AddTile(TileID.WorkBenches).Register();
             Recipe.Create(ModContent.ItemType<AshesofCalamity>(), 10).AddIngredient(Type).AddTile(TileID.WorkBenches).Register();
+        }
+        public override bool? UseItem(Player player)
+        {
+            Vector2 ritualSpawnPosition = player.Center;
+            SoundEngine.PlaySound(SCalAltar.SummonSound, ritualSpawnPosition);
+            Projectile.NewProjectile(new EntitySource_WorldEvent(), ritualSpawnPosition, Vector2.Zero, ModContent.ProjectileType<SCalRitualDrama>(), 0, 0f, Main.myPlayer, 0, 0);
+            return true;
+        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            return false;
         }
     }
 }
