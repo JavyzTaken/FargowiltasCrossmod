73a1b1626dc3915c2c88a5ff6ae8e6d59c70d001
JavyzTaken <jacklukas@outlook.com>
2024-04-07 01:34:55 +0200

    Eternity Brimstone Elemental: Projectiles now have black borders

--- Content/Calamity/Bosses/BrimstoneElemental/BEGlobalProjectile.cs
+++ Content/Calamity/Bosses/BrimstoneElemental/BEGlobalProjectile.cs
@@ -6,10 +6,6 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Terraria;
 using Terraria.GameContent;
 using Terraria.ModLoader;
@@ -36,9 +32,20 @@ namespace FargowiltasCrossmod.Content.Calamity.Bosses.BrimstoneElemental
                 {
                     frameSize = new Vector2(36, 50);
                 }
-                DLCUtils.DrawBackglow(t, new Color(100, 100, 100), projectile.Center, frameSize / 2, projectile.rotation, projectile.scale, offsetMult: 2, sourceRectangle: new Rectangle(0, (int)frameSize.Y * projectile.frame, (int)frameSize.X, (int)frameSize.Y));
+                DrawBlackBorder(t, projectile.Center, frameSize / 2, projectile.rotation, projectile.scale, offsetMult: 3, sourceRectangle: new Rectangle(0, (int)frameSize.Y * projectile.frame, (int)frameSize.X, (int)frameSize.Y));
             }
             return base.PreDraw(projectile, ref lightColor);
+        }
+        public static void DrawBlackBorder(Asset<Texture2D> texture, Vector2 position, Vector2 origin, float rotation = 0, float scale = 1, SpriteEffects spriteEffects = SpriteEffects.None, int iterations = 12, float offsetMult = 1, Rectangle? sourceRectangle = null)
+        {
+            for (int j = 0; j < iterations; j++)
+            {
+                Vector2 afterimageOffset = (MathHelper.TwoPi * j / 12f).ToRotationVector2() * offsetMult;
+                Color glowColor = new(40, 24, 48);
+                glowColor *= 0.7f;
+
+                Main.EntitySpriteDraw(texture.Value, position + afterimageOffset - Main.screenPosition, sourceRectangle, glowColor, rotation, origin, scale, spriteEffects);
+            }
         }
     }
 }
