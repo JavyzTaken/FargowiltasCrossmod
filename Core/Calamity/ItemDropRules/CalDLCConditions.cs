5d53f92877db7fa055cc4807b5c34c91811ca1d5
JavyzTaken <jacklukas@outlook.com>
2024-05-24 13:46:25 +0200

    Renamed more files.

--- Core/Calamity/ItemDropRules/CalDLCConditions.cs
+++ Core/Calamity/ItemDropRules/CalDLCConditions.cs
@@ -6,7 +6,7 @@ namespace FargowiltasCrossmod.Core.Calamity.ItemDropRules
 {
     [ExtendsFromMod(ModCompatibility.Calamity.Name)]
     [JITWhenModsEnabled(ModCompatibility.Calamity.Name)]
-    public static class FargoCalConditions
+    public static class CalDLCConditions
     {
         public static Condition RevNotEmodeCondition = new("In Revengeance but not Eternity Mode", () => ModCompatibility.Calamity.Mod?.Code.GetType("CalamityMod.World.CalamityWorld")?.GetField("revenge")?.GetValue(null) is bool revenge && revenge && !WorldSavingSystem.EternityMode);
         public static Condition EmodeNotRevCondition = new("In Eternity Mode but not Revengeance", () => ModCompatibility.Calamity.Mod?.Code.GetType("CalamityMod.World.CalamityWorld")?.GetField("revenge")?.GetValue(null) is bool revenge && !revenge && WorldSavingSystem.EternityMode);
