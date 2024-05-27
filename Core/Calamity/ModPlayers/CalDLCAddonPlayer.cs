e7b134627e313bdc6aaa26972b4e2ddaaa9832c3
JavyzTaken <jacklukas@outlook.com>
2024-05-27 21:14:16 +0200

    Desert Prowler: Now triggered by holding UP, now has a flat horizontal speedup effect

--- Core/Calamity/ModPlayers/CalDLCAddonPlayer.cs
+++ Core/Calamity/ModPlayers/CalDLCAddonPlayer.cs
@@ -38,7 +38,6 @@ namespace FargowiltasCrossmod.Core.Calamity.ModPlayers
         public int NumJumpsUsed = 0;
         public bool AllowJumpsUsedInc = false;
         public bool RuffianModifiedRotation = false;
-        public bool DesertProwlerRocketPower = false;
         public override bool IsLoadingEnabled(Mod mod)
         {
             //return FargowiltasCrossmod.EnchantLoadingEnabled;
