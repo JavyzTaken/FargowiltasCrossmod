661461ef12794a3034abf682272affcd22403539
JavyzTaken <jacklukas@outlook.com>
2024-06-14 22:18:05 +0200

    Decreased Eternity-Rev+Skeletron hand damage (it doubled dipped on Eternity and Calamity buffs)

--- Core/Calamity/Globals/CalDLCNPCChanges.cs
+++ Core/Calamity/Globals/CalDLCNPCChanges.cs
@@ -184,7 +184,10 @@ namespace FargowiltasCrossmod.Core.Calamity.Globals
                         break;
                     case NPCID.SkeletronHand:
                         if (CalDLCWorldSavingSystem.E_EternityRev)
+                        {
                             npc.lifeMax = (int)Math.Round(npc.lifeMax * 1.3f);
+                            npc.damage = 36;
+                        }
                         break;
                     case NPCID.BrainofCthulhu:
                         npc.lifeMax = (int)(npc.lifeMax * 0.65f);
