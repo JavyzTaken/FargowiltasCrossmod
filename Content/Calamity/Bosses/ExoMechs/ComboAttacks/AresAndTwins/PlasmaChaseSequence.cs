cddbc6ca0b335984d5543ce04ec77601753aa912
Dominic Karma <farfetchedking541@gmail.com>
2024-06-30 12:56:17 -0500

    Add Exoplasma Flame Jet

---
+++ Content/Calamity/Bosses/ExoMechs/ComboAttacks/AresAndTwins/PlasmaChaseSequence.cs
@@ -0,0 +1,156 @@
+﻿using CalamityMod.NPCs.ExoMechs.Apollo;
+using CalamityMod.NPCs.ExoMechs.Ares;
+using CalamityMod.Sounds;
+using FargowiltasCrossmod.Content.Calamity.Bosses.ExoMechs.Ares;
+using FargowiltasCrossmod.Content.Calamity.Bosses.ExoMechs.FightManagers;
+using FargowiltasCrossmod.Content.Calamity.Bosses.ExoMechs.Projectiles;
+using FargowiltasCrossmod.Core;
+using FargowiltasCrossmod.Core.Calamity;
+using Luminance.Common.Utilities;
+using Luminance.Core.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FargowiltasCrossmod.Content.Calamity.Bosses.ExoMechs.ComboAttacks
+{
+    [JITWhenModsEnabled(ModCompatibility.Calamity.Name)]
+    [ExtendsFromMod(ModCompatibility.Calamity.Name)]
+    public class PlasmaChaseSequence : ExoMechComboHandler
+    {
+        /// <summary>
+        /// How long Ares spends charging up energy for his plasma cannons.
+        /// </summary>
+        public static int PlasmaCannonChargeUpTime => LumUtils.SecondsToFrames(2.62f);
+
+        /// <summary>
+        /// How much damage plasma flame jets from Ares' plasma cannons do.
+        /// </summary>
+        public static int PlasmaJetDamage => Main.expertMode ? 500 : 360;
+
+        /// <summary>
+        /// The speed at which Ares chases his target.
+        /// </summary>
+        public static float AresChaseSpeedInterpolant => 0.033f;
+
+        public override int[] ExpectedManagingExoMechs => [ModContent.NPCType<AresBody>(), ModContent.NPCType<Apollo>()];
+
+        public override bool Perform(NPC npc)
+        {
+            if (npc.type == ModContent.NPCType<AresBody>())
+                Perform_Ares(npc);
+
+            return false;
+        }
+
+        /// <summary>
+        /// Performs Ares' part in the PlasmaChaseSequence attack.
+        /// </summary>
+        /// <param name="npc">Ares' NPC instance.</param>
+        public static void Perform_Ares(NPC npc)
+        {
+            if (!npc.TryGetDLCBehavior(out AresBodyEternity ares))
+            {
+                npc.active = false;
+                return;
+            }
+
+            if (Main.mouseRight && Main.mouseRightRelease)
+            {
+                ExoMechFightStateManager.ClearExoMechProjectiles();
+                AITimer = 0;
+            }
+
+            if (AITimer == Math.Max(1, PlasmaCannonChargeUpTime - LumUtils.SecondsToFrames(2.6f)))
+                SoundEngine.PlaySound(AresPlasmaFlamethrower.TelSound);
+
+            if (AITimer <= PlasmaCannonChargeUpTime)
+            {
+                npc.velocity *= 0.95f;
+                ares.AnimationState = AresBodyEternity.AresFrameAnimationState.Default;
+            }
+            else
+            {
+                float moveSpeedInterpolant = LumUtils.InverseLerp(0f, 45f, AITimer - PlasmaCannonChargeUpTime);
+                Vector2 hoverDestination = Target.Center + new Vector2(npc.OnRightSideOf(Target).ToDirectionInt() * 150f, -450f);
+                npc.SmoothFlyNear(hoverDestination, moveSpeedInterpolant * AresChaseSpeedInterpolant, 0.92f);
+
+                float moveAbovePlayerInterpolant = LumUtils.InverseLerp(400f, 150f, Target.Center.Y - npc.Center.Y) + 0.2f;
+                npc.Center = Vector2.Lerp(npc.Center, new(npc.Center.X, hoverDestination.Y), moveAbovePlayerInterpolant * 0.2f);
+                ares.AnimationState = AresBodyEternity.AresFrameAnimationState.Laugh;
+            }
+
+            if (AITimer == PlasmaCannonChargeUpTime)
+            {
+                SoundEngine.PlaySound(AresBodyEternity.LaughSound);
+                SoundEngine.PlaySound(CommonCalamitySounds.ExoPlasmaShootSound);
+                SoundEngine.PlaySound(CommonCalamitySounds.ExoPlasmaExplosionSound);
+                ScreenShakeSystem.StartShake(10f);
+
+                npc.velocity.Y -= 54f;
+                npc.netUpdate = true;
+            }
+
+            npc.rotation = npc.velocity.X * 0.0062f;
+
+            float chargeUpInterpolant = LumUtils.InverseLerp(0f, PlasmaCannonChargeUpTime, AITimer);
+            float handRepositionInterpolant = chargeUpInterpolant.Squared();
+            Vector2 farLeftHandOffset = Vector2.SmoothStep(new Vector2(-448f, 90f), new Vector2(-400f, 50f), handRepositionInterpolant);
+            Vector2 leftHandOffset = Vector2.SmoothStep(new Vector2(-250f, 220f), new Vector2(-140f, 274f), handRepositionInterpolant);
+            Vector2 rightHandOffset = Vector2.SmoothStep(new Vector2(250f, 220f), new Vector2(140f, 274f), handRepositionInterpolant);
+            Vector2 farRightHandOffset = Vector2.SmoothStep(new Vector2(448f, 90f), new Vector2(400f, 50f), handRepositionInterpolant);
+
+            ares.InstructionsForHands[0] = new(h => AresHandUpdate(npc, h, farLeftHandOffset, 0));
+            ares.InstructionsForHands[1] = new(h => AresHandUpdate(npc, h, leftHandOffset, 1));
+            ares.InstructionsForHands[2] = new(h => AresHandUpdate(npc, h, rightHandOffset, 2));
+            ares.InstructionsForHands[3] = new(h => AresHandUpdate(npc, h, farRightHandOffset, 3));
+        }
+
+        /// <summary>
+        /// Handles updating for Ares' hands during the PlasmaChaseSequence attack.
+        /// </summary>
+        /// <param name="aresBody">Ares' NPC instance.</param>
+        /// <param name="hand">The hand ModNPC instance.</param>
+        /// <param name="hoverOffset">The offset for the hand relative to the body's position.</param>
+        /// <param name="armIndex">The arm's index in the overall set.</param>
+        public static void AresHandUpdate(NPC aresBody, AresHand hand, Vector2 hoverOffset, int armIndex)
+        {
+            NPC handNPC = hand.NPC;
+            Vector2 hoverDestination = aresBody.Center + hoverOffset * aresBody.scale;
+
+            hand.KatanaInUse = true;
+            hand.UsesBackArm = armIndex == 0 || armIndex == AresBodyEternity.ArmCount - 1;
+            hand.ArmSide = (armIndex >= AresBodyEternity.ArmCount / 2).ToDirectionInt();
+            hand.HandType = AresHandType.PlasmaCannon;
+            hand.ArmEndpoint = handNPC.Center + handNPC.velocity;
+            hand.GlowmaskDisabilityInterpolant = 0f;
+            hand.Frame = AITimer / 4 % 12;
+            handNPC.spriteDirection = -hand.ArmSide;
+            handNPC.Opacity = LumUtils.Saturate(handNPC.Opacity + 0.3f);
+
+            hand.EnergyDrawer.chargeProgress = LumUtils.InverseLerp(0f, PlasmaCannonChargeUpTime * 0.85f, AITimer);
+
+            float idealRotation = handNPC.AngleTo(aresBody.Center + Vector2.UnitY * 400f).AngleLerp(MathHelper.PiOver2, hand.EnergyDrawer.chargeProgress);
+            if (handNPC.spriteDirection == -1)
+                idealRotation += MathHelper.Pi;
+
+            handNPC.rotation = handNPC.rotation.AngleLerp(idealRotation, 0.1f);
+
+            if (AITimer >= PlasmaCannonChargeUpTime)
+            {
+                hand.Frame += 24;
+                hand.EnergyDrawer.chargeProgress = 0f;
+                handNPC.velocity += Main.rand.NextVector2Circular(1f, 2f);
+            }
+
+            handNPC.SmoothFlyNear(hoverDestination, 0.35f, 0.65f);
+
+            Vector2 cannonEnd = handNPC.Center + new Vector2(handNPC.spriteDirection * 54f, 8f).RotatedBy(handNPC.rotation);
+            if (Main.netMode != NetmodeID.MultiplayerClient && AITimer == PlasmaCannonChargeUpTime)
+                LumUtils.NewProjectileBetter(handNPC.GetSource_FromAI(), cannonEnd, Vector2.UnitY, ModContent.ProjectileType<PlasmaFlameJet>(), PlasmaJetDamage, 0f, -1, handNPC.whoAmI);
+        }
+    }
+}
---
+++ Content/Calamity/Bosses/ExoMechs/Projectiles/PlasmaFlameJet.cs
@@ -0,0 +1,193 @@
+﻿using CalamityMod.DataStructures;
+using CalamityMod.NPCs.ExoMechs.Ares;
+using CalamityMod.Particles;
+using FargowiltasCrossmod.Content.Calamity.Bosses.ExoMechs.Ares;
+using FargowiltasCrossmod.Content.Calamity.Bosses.ExoMechs.SpecificManagers;
+using FargowiltasCrossmod.Core;
+using Luminance.Assets;
+using Luminance.Common.DataStructures;
+using Luminance.Common.Utilities;
+using Luminance.Core.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FargowiltasCrossmod.Content.Calamity.Bosses.ExoMechs.Projectiles
+{
+    [JITWhenModsEnabled(ModCompatibility.Calamity.Name)]
+    [ExtendsFromMod(ModCompatibility.Calamity.Name)]
+    public class PlasmaFlameJet : ModProjectile, IPixelatedPrimitiveRenderer, IProjOwnedByBoss<AresBody>, IExoMechProjectile
+    {
+        public PixelationPrimitiveLayer LayerToRenderTo => PixelationPrimitiveLayer.AfterProjectiles;
+
+        /// <summary>
+        /// The owner of this flame jet.
+        /// </summary>
+        public NPC Owner
+        {
+            get
+            {
+                int ownerIndex = (int)Projectile.ai[0];
+                if (ownerIndex <= -1 || ownerIndex >= Main.maxNPCs)
+                    return null;
+
+                NPC owner = Main.npc[ownerIndex];
+                if (!owner.active || owner.type != ModContent.NPCType<AresHand>())
+                    return null;
+
+                return owner;
+            }
+        }
+
+        /// <summary>
+        /// The set of control points that compose this flame jet.
+        /// </summary>
+        public Vector2[] ControlPoints
+        {
+            get
+            {
+                float curvatureInterpolant = 0.4f;
+                Vector2[] controlPoints = new Vector2[Projectile.oldPos.Length];
+                for (int i = 0; i < controlPoints.Length; i++)
+                {
+                    controlPoints[i] = Projectile.Center + Vector2.UnitY * i / (float)(controlPoints.Length - 1f) * JetLength;
+
+                    if (Projectile.oldPos[i] != Vector2.Zero)
+                        controlPoints[i].X = MathHelper.Lerp(controlPoints[i].X, Projectile.oldPos[i].X + Projectile.width * 0.5f, curvatureInterpolant);
+                }
+                return controlPoints;
+            }
+        }
+
+        /// <summary>
+        /// How long this flame jet has existed, in frames.
+        /// </summary>
+        public ref float Time => ref Projectile.ai[1];
+
+        /// <summary>
+        /// The length of this flame jet.
+        /// </summary>
+        public ref float JetLength => ref Projectile.ai[2];
+
+        /// <summary>
+        /// The maximum length of this jet.
+        /// </summary>
+        public static float MaxJetLength => 2400f;
+
+        public override string Texture => MiscTexturesRegistry.InvisiblePixelPath;
+
+        public ExoMechDamageSource DamageType => ExoMechDamageSource.Plasma;
+
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailingMode[Type] = 2;
+            ProjectileID.Sets.TrailCacheLength[Type] = 32;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 148;
+            Projectile.height = 148;
+            Projectile.penetrate = -1;
+            Projectile.tileCollide = false;
+            Projectile.ignoreWater = true;
+            Projectile.hostile = true;
+            Projectile.timeLeft = 9000000;
+            CooldownSlot = ImmunityCooldownID.Bosses;
+        }
+
+        public override void AI()
+        {
+            if (Owner is null)
+            {
+                Projectile.Kill();
+                return;
+            }
+
+            Vector2 cannonEnd = Owner.Center + new Vector2(Owner.spriteDirection * 58f, 16f).RotatedBy(Owner.rotation);
+            Projectile.Center = cannonEnd;
+            Projectile.velocity = Vector2.UnitY;
+            JetLength = MathHelper.Clamp(JetLength + 150f, 0f, MaxJetLength);
+
+            BezierCurve shapeCurve = new(ControlPoints);
+            for (int i = 0; i < 10; i++)
+            {
+                float particleScale = 2f;
+                Color fireColor = Color.Lerp(Color.Lime, new(255, 255, 193), Main.rand.NextFloat(0.7f));
+                Vector2 fireSpawnPosition = shapeCurve.Evaluate(Main.rand.NextFloat(0.06f, 1f));
+                var particle = new HeavySmokeParticle(fireSpawnPosition, Main.rand.NextVector2Circular(20f, 7f), fireColor, 26, particleScale, 1f, 0.03f, true, -0.0045f, true);
+                GeneralParticleHandler.SpawnParticle(particle);
+            }
+
+            Time++;
+        }
+
+        /// <summary>
+        /// The width function for the flame jet primitives.
+        /// </summary>
+        /// <param name="completionRatio">How far along the trail the sampled position is.</param>
+        public float FlameJetWidthFunction(float completionRatio)
+        {
+            float widthFactor = MathHelper.Lerp(0.75f, 1f, completionRatio) * JetLength / MaxJetLength;
+            return Projectile.width * widthFactor;
+        }
+
+        /// <summary>
+        /// The color function for the flame jet primitives.
+        /// </summary>
+        /// <param name="completionRatio">How far along the trail the sampled position is.</param>
+        public Color FlameJetColorFunction(float completionRatio) => new Color(97, 206, 74) * Projectile.Opacity;
+
+        /// <summary>
+        /// The width function for the flame jet bloom primitives.
+        /// </summary>
+        /// <param name="completionRatio">How far along the trail the sampled position is.</param>
+        public float FlameJetBloomWidthFunction(float completionRatio)
+        {
+            float widthFactor = MathHelper.SmoothStep(0f, 1f, LumUtils.InverseLerp(0.012f, 0.09f, completionRatio)) * 1.4f;
+            return FlameJetWidthFunction(completionRatio) * widthFactor;
+        }
+
+        /// <summary>
+        /// The color function for the flame jet bloom primitives.
+        /// </summary>
+        /// <param name="completionRatio">How far along the trail the sampled position is.</param>
+        public Color FlameJetBloomColorFunction(float completionRatio)
+        {
+            float opacity = Projectile.Opacity * LumUtils.InverseLerp(0.94f, 0.8f, completionRatio);
+            return new Color(99, 204, 5) * opacity;
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            ManagedShader shader = ShaderManager.GetShader("FargowiltasCrossmod.PrimitiveBloomShader");
+            shader.TrySetParameter("innerGlowIntensity", 0.29f);
+
+            PrimitiveSettings bloomSettings = new(FlameJetBloomWidthFunction, FlameJetBloomColorFunction, Shader: shader);
+            PrimitiveRenderer.RenderTrail(ControlPoints, bloomSettings, 42);
+            return false;
+        }
+
+        public void RenderPixelatedPrimitives(SpriteBatch spriteBatch)
+        {
+            ManagedShader flameShader = ShaderManager.GetShader("FargowiltasCrossmod.PlasmaFlameJetShader");
+            flameShader.SetTexture(MiscTexturesRegistry.WavyBlotchNoise.Value, 1, SamplerState.LinearWrap);
+            flameShader.TrySetParameter("localTime", Main.GlobalTimeWrappedHourly + Projectile.identity * 0.412f);
+
+            PrimitiveSettings settings = new(FlameJetWidthFunction, FlameJetColorFunction, _ => Vector2.Zero, Pixelate: true, Shader: flameShader);
+            PrimitiveRenderer.RenderTrail(ControlPoints, settings, 35);
+        }
+
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            float _ = 0f;
+            Vector2 start = Projectile.Center;
+            Vector2 end = start + Projectile.velocity * JetLength;
+            return Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), start, end, Projectile.width * 0.6f, ref _);
+        }
+
+        public override bool ShouldUpdatePosition() => false;
+    }
+}
