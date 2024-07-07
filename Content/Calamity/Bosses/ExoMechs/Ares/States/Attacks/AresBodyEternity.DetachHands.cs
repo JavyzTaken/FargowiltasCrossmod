7a90b7fbe6961be9b0f14dddc7675b6e39be8525
Dominic Karma <farfetchedking541@gmail.com>
2024-07-07 01:46:57 -0500

    Attack state selection tweak

--- Content/Calamity/Bosses/ExoMechs/Ares/States/Attacks/AresBodyEternity.DetachHands.cs
+++ Content/Calamity/Bosses/ExoMechs/Ares/States/Attacks/AresBodyEternity.DetachHands.cs
@@ -31,7 +31,7 @@ namespace FargowiltasCrossmod.Content.Calamity.Bosses.ExoMechs.Ares
                     if (Main.rand.NextBool() && ExoMechFightStateManager.CurrentPhase >= ExoMechFightDefinitions.BerserkSoloPhaseDefinition)
                         CurrentState = AresAIState.BackgroundCoreLaserBeams;
                     if (Main.rand.NextBool() && (NPC.life <= NPC.lifeMax * ExoMechFightDefinitions.FightAloneLifeRatio || ExoMechFightStateManager.CurrentPhase >= ExoMechFightDefinitions.BerserkSoloPhaseDefinition))
-                        CurrentState = AresAIState.BackgroundCoreLaserBeams;
+                        CurrentState = AresAIState.KatanaCycloneDashes;
                 }
                 while (CurrentState == PreviousState);
 
