6a8e2dfd561da94798bfbab27b700d6a114a6983
JavyzTaken <jacklukas@outlook.com>
2024-09-07 17:12:16 +0200

    Golem HP x0.35 -> x0.3, Golem Head HP 1x -> 0.9x

--- Core/Calamity/Globals/CalDLCNPCChanges.cs
+++ Core/Calamity/Globals/CalDLCNPCChanges.cs
@@ -199,7 +199,10 @@ namespace FargowiltasCrossmod.Core.Calamity.Globals
                         npc.lifeMax = (int)(npc.lifeMax * 0.375f);
                         break;
                     case NPCID.Golem:
-                        npc.lifeMax = (int)(npc.lifeMax * 0.35f);
+                        npc.lifeMax = (int)(npc.lifeMax * 0.3f);
+                        break;
+                    case NPCID.GolemHead:
+                        npc.lifeMax = (int)(npc.lifeMax * 0.9f);
                         break;
                     case NPCID.DukeFishron:
                         npc.lifeMax = (int)(npc.lifeMax * 0.5f);
