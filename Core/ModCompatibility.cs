d76ecf88ca7849af8f2fd71d3e38c4c9ed6bf2a1
JavyzTaken <jacklukas@outlook.com>
2024-10-20 23:21:42 +0200

    (HotOG) Removed Splendor Jam from Brand of the Brimstone Witch

--- Core/ModCompatibility.cs
+++ Core/ModCompatibility.cs
@@ -50,12 +50,6 @@ public static class ModCompatibility
         public static ModNPC NoxusBoss2 = Mod.Find<ModNPC>(Mod.Version >= new Version(1, 2, 0) ? "AvatarOfEmptiness" : "EntropicGod");
         public static ModNPC NamelessDeityBoss = Mod.Find<ModNPC>("NamelessDeityBoss");
     }
-    public static class CalamityHunt
-    {
-        public const string Name = "CalamityHunt";
-        public static bool Loaded => ModLoader.HasMod(Name);
-        public static Mod Mod => ModLoader.GetMod(Name);
-    }
     public static class BossChecklist
     {
         public static void AdjustValues()
