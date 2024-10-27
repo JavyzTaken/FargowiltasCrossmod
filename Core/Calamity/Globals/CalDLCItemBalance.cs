5f2eb35c265ad83a9d5a05cea674827e20ebc855
JavyzTaken <jacklukas@outlook.com>
2024-10-27 16:41:56 +0100

    Blender damage 0.8x

--- Core/Calamity/Globals/CalDLCItemBalance.cs
+++ Core/Calamity/Globals/CalDLCItemBalance.cs
@@ -51,7 +51,7 @@ namespace FargowiltasCrossmod.Core.Calamity.Globals
             if (item.type == ModContent.ItemType<MechanicalLeashOfCthulhu>())
                 return 0.5f;
             if (item.type == ModContent.ItemType<Blender>())
-                return 1f;
+                return 0.8f;
             if (item.type == ModContent.ItemType<NukeFishron>() || item.type == ModContent.ItemType<GolemTome2>() || item.type == ModContent.ItemType<DestroyerGun2>())
                 return 2f;
 
