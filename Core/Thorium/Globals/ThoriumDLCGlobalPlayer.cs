3259e99e4f9a7ff670a82da1a2199f5275d68d0d
Ghoose <78841748+Ghoose1@users.noreply.github.com>
2024-11-18 16:12:39 +1000

    Nerfed empowerment stacking
    Now you can only stack up to 4 empowerments in emode
    
    updated ThoriumMod.dll

---
+++ Core/Thorium/Globals/ThoriumDLCGlobalPlayer.cs
@@ -0,0 +1,44 @@
+using System.Linq;
+using System.Reflection;
+using Terraria;
+using Terraria.ModLoader;
+using ThoriumMod.Empowerments;
+
+namespace FargowiltasCrossmod.Core.Thorium.Globals
+{
+    [ExtendsFromMod(Core.ModCompatibility.ThoriumMod.Name)]
+    public class ThoriumPlayerBalance : ModPlayer
+    {
+        private const int MAX_EMPOWERMENTS = 4;
+        public override void PostUpdate()
+        {
+            if (FargowiltasSouls.Core.Systems.WorldSavingSystem.EternityMode)
+                UnstackEmpowerments();
+        }
+
+        private void UnstackEmpowerments()
+        {
+            var thoriumPlayer = Player.Thorium();
+
+            var field = typeof(ThoriumMod.ThoriumPlayer).GetField("Empowerments", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (field == null) return;
+            
+            var value = field.GetValue(thoriumPlayer);
+            if (value is not EmpowermentData data) return;
+
+            if (data.ActiveEmpowerments.Count <= MAX_EMPOWERMENTS) return;
+
+            data.Timers[data.ActiveEmpowerments[0]].timer = 0;
+            // data.ActiveEmpowerments.RemoveAt(0);
+
+            // data.Timers.OrderBy(timer => timer.Value.timer).First().Value.timer = 0;
+            // var empToRemove = data.Timers.OrderBy(timer => timer.Value.timer).First();
+            // empToRemove.Value.timer = 0;
+            // data.ActiveEmpowerments.Remove(empToRemove.Value.Type);
+            // data.Timers.Remove(empToRemove.Key);
+            // data.Timers[empToRemove.Key].level = 0;
+            // data.Timers[empToRemove.Key].Update();
+            // field.SetValue(thoriumPlayer, data);
+        }
+    }
+}
