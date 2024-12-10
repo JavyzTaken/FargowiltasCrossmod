dd7d6a41e296b464ad160a974b26cdc54bc97db9
Ghoose <78841748+Ghoose1@users.noreply.github.com>
2024-12-10 15:07:56 +1000

    Nerfed Scythe of the undoing  - Lifesteal now has a 0.5 second cd, meaning only 6 lifesteal / s

---
+++ Content/Thorium/Buffs/ScytheOfUndoingLifestealCD.cs
@@ -0,0 +1,14 @@
+using Terraria;
+using Terraria.ModLoader;
+
+namespace FargowiltasCrossmod.Content.Thorium.Buffs;
+
+[ExtendsFromMod(Core.ModCompatibility.ThoriumMod.Name)]
+public class ScytheOfUndoingLifestealCD : ModBuff
+{
+    public override string Texture => "FargowiltasSouls/Content/Buffs/PlaceholderDebuff";
+    public override void SetStaticDefaults()
+    {
+        Main.debuff[Type] = true;
+    }
+}
---
+++ Core/Thorium/Globals/ScytheOfUndoingPro2GlobalProj.cs
@@ -0,0 +1,40 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using ThoriumMod;
+using ThoriumMod.Buffs.Healer;
+using ThoriumMod.Projectiles;
+using ThoriumMod.Utilities;
+
+namespace FargowiltasCrossmod.Core.Thorium.Globals;
+
+[ExtendsFromMod(Core.ModCompatibility.ThoriumMod.Name)]
+public class ScytheOfUndoingPro2GlobalProj : GlobalProjectile
+{
+    public override void OnHitNPC(Projectile projectile, NPC target, NPC.HitInfo hit, int damageDone)
+    {
+		Player player = Main.player[projectile.owner];
+		if (!target.friendly && target.lifeMax > 5 && target.chaseable && (!target.dontTakeDamage) && !target.immortal)
+		{
+			if (!player.HasBuff<Content.Thorium.Buffs.ScytheOfUndoingLifestealCD>())
+			{
+				player.AddBuff(ModContent.BuffType<Content.Thorium.Buffs.ScytheOfUndoingLifestealCD>(), 30);
+				int heal = 3;
+				player.HealLife(heal);
+				IEntitySource source_OnHit = projectile.GetSource_OnHit(target);
+				Projectile.NewProjectile(source_OnHit, target.Center.X, target.Center.Y, Main.rand.NextFloat(-3f, 3f), Main.rand.NextFloat(-3f, 3f), ModContent.ProjectileType<VampireScepterPro2>(), 0, 0f, projectile.owner);
+			}
+			
+			int charge = 2;
+			ThoriumPlayer thoriumPlayer = player.GetThoriumPlayer();
+			if (projectile.ai[0] <= 0f)
+			{
+				player.AddBuff(ModContent.BuffType<SoulEssence>(), 1800);
+				CombatText.NewText(target.Hitbox, new Color(100, 255, 200), charge, dramatic: false, dot: true);
+				thoriumPlayer.soulEssence += charge;
+				projectile.ai[0] = 30f;
+			}
+		}
+    }
+}
