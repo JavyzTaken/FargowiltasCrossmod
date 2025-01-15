4d16527903ba43f38e50c77d1c7ca05f7bb5757c
Dominic Karma <farfetchedking541@gmail.com>
2025-01-15 13:01:01 -0600

    Hades' head is now always vulnerable

--- Content/Calamity/Bosses/ExoMechs/Hades/HadesHeadEternity.cs
+++ Content/Calamity/Bosses/ExoMechs/Hades/HadesHeadEternity.cs
@@ -400,7 +400,7 @@ namespace FargowiltasCrossmod.Content.Calamity.Bosses.ExoMechs.Hades
             DisableMapIcon = false;
             SegmentReorientationStrength = MathHelper.Lerp(SegmentReorientationStrength, 1f, 0.03f);
             NPC.As<ThanatosHead>().SecondaryAIState = (int)ThanatosHead.SecondaryPhase.Nothing;
-            SegmentOpenInterpolant = Utilities.Saturate(SegmentOpenInterpolant - StandardSegmentOpenRate);
+            SegmentOpenInterpolant = Utilities.Saturate(SegmentOpenInterpolant + StandardSegmentOpenRate);
             JawRotation = JawRotation.AngleLerp(0f, 0.01f).AngleTowards(0f, 0.02f);
             ReticleOpacity = MathHelper.Lerp(ReticleOpacity, 0f, 0.1f);
 
