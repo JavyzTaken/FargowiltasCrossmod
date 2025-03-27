a055e99f3d1bb202c4fc08bf6340e7059f1e8b67
Dominic Karma <farfetchedking541@gmail.com>
2025-03-27 15:26:43 -0500

    Fix enigmatic Artemis/Apollo disappearance bug

--- Content/Calamity/Bosses/ExoMechs/ComboAttacks/ExoMechComboAttackManager.cs
+++ Content/Calamity/Bosses/ExoMechs/ComboAttacks/ExoMechComboAttackManager.cs
@@ -233,6 +233,12 @@ namespace FargowiltasCrossmod.Content.Calamity.Bosses.ExoMechs.ComboAttacks
                 if (!ExoMechNPCIDs.ExoMechIDs.Contains(npc.type) || !npc.TryGetDLCBehavior(out CalDLCEmodeBehavior behavior))
                     continue;
 
+                // This accounts for one-frame discrepancies when changing states, ensuring that only Exo Mechs that are intended for a given combo execute said combo.
+                // Cases have arisen where Artemis and Apollo disappear due to running the SuperchargedTurbodashes state for one frame before the mayhem phase begins, and then disappear,
+                // since they aren't supposed to be involved during the turbodashes attack, Hades and Ares are.
+                if (!ExoMechFightStateManager.ActiveManagingExoMechs.Contains(behavior))
+                    continue;
+
                 // Check if any of the Exo Mechs want to sync.
                 // If they do, that could mean a time sensitive event has occurred, and as such it's best to be safe and sync the timer.
                 if (npc.netUpdate)
